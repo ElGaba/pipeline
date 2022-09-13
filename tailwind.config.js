ffa705682c0519a89d609dae3702b2fab75a31ff
Gabael Paez Castañeda <gabael.paez@gmail.com>
2022-09-12 23:31:46 -0500

    Modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    content: [
+    purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
     ],
 
     theme: {
@@ -15,6 +12,12 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
         },
     },
 
